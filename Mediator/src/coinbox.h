b23d82d005e73502eae20d99dfa9885c4f5e1fa4
daum <daum@DAUM212.daumschool.co.kr>
2018-01-12 11:37:14 +0900

    Mediator 패턴

---
+++ Mediator/src/coinbox.h
@@ -0,0 +1,40 @@
+/*
+ * coinbox.h
+ *
+ *  Created on: 2018. 1. 12.
+ *      Author: daum
+ */
+
+#ifndef COINBOX_H_
+#define COINBOX_H_
+
+class Mediator;
+
+class CoinBox{
+#define MAX_COIN_CNT 500
+#define MIN_COIN_CNT 10
+
+#define COIN_ACCEPT_MODE 1
+#define COIN_REJECT_MODE 0
+
+public:
+	CoinBox();
+	void SetMediator(Mediator* pMediator);
+
+	void InsertCoin();
+	void RejectCoin();
+	void PayOutCoin(int payOutCnt);
+	void SetCoinBoxMode(int mode);
+
+protected:
+	bool IsValid();
+
+private:
+	Mediator* pMediator_;
+	int coinCnt_;
+	int mode_;
+};
+
+
+
+#endif /* COINBOX_H_ */
---
+++ Mediator/src/main.cpp
@@ -0,0 +1,38 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2018. 1. 12.
+ *      Author: daum
+ */
+
+#include <iostream>
+#include "lamp.h"
+#include "mixer.h"
+#include "coinbox.h"
+#include "billbox.h"
+#include "mediator.h"
+
+using namespace std;
+
+int main(){
+	Lamp lamp;
+	Mixer mixer;
+	CoinBox coinBox;
+	BillBox billBox;
+
+	Mediator mediator(&lamp,&mixer,&coinBox,&billBox);
+	mixer.SetMediator(&mediator);
+	coinBox.SetMediator(&mediator);
+	billBox.SetMediator(&mediator);
+
+	for(int i=0; i<90; i++) coinBox.InsertCoin();
+
+	mixer.OutOfOrder();
+	coinBox.InsertCoin();
+	billBox.InsertBill();
+	mixer.Repair();
+
+	billBox.InsertBill(8);
+}
+
+
